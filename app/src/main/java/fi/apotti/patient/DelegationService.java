f8a60bb4e79369a8a55f389478742eceb85a387f
Jari Vuonos <jari.vuonos@gmail.com>
2026-08-12 15:20:35 +0300

    assetlinks update

---
+++ app/src/main/java/fi/apotti/patient/DelegationService.java
@@ -0,0 +1,14 @@
+package fi.apotti.patient;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
